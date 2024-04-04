9787d574d0fb83b3987b5a8b6c5ead787cc32c93
Saeed Heydari <saeedhpro@gmail.com>
2024-04-04 03:53:59 +0330

    feature: install api

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function metas()
+    {
+        return $this->morphMany(Meta::class, 'metaable');
+    }
 }
