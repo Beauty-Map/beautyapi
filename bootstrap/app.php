8c0cb05dd17042c552dca712009457cb825d0cc9
Saeed Heydari <saeedhpro@gmail.com>
2024-09-16 12:26:28 +0330

    fix middlware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(
             except: ['*']
         );
-        $middleware->append(AdminTokenMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
