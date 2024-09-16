93d953ca1cdf663a5a2690932502d0a69c0cc795
Saeed Heydari <saeedhpro@gmail.com>
2024-09-16 21:06:03 +0330

    fix serve

--- app/Listeners/SendRegisterOtpListener.php
+++ app/Listeners/SendRegisterOtpListener.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendRegisterOtpListener implements ShouldQueue
+class SendRegisterOtpListener
 {
     /**
      * Create the event listener.
