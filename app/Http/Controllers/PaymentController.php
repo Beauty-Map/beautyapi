3f3f97bf4866efe506cbf1c73e8d1000c998de32
Saeed Heydari <saeedhpro@gmail.com>
2024-10-13 18:21:35 +0330

    f

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -43,7 +43,7 @@ class PaymentController extends Controller
 
         $price = $paymentOption->price;
 //        $price *=  10 ** 9;
-        $price = 100 * 10 ** 9;
+        $price = 100;
         $walletAddress = "UQCdN7eLj50eFEvwNBAkrvEN3Z8_ETg60uAfxfQI4a3L7BCR";
         $paymentLink = "ton://transfer/$walletAddress?amount=$price&text=$transactionId&comment=$transactionId";
 
