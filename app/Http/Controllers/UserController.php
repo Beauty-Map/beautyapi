2098f4a5a9cf74377d59387c2b185884093cd186
Saeed Heydari <saeedhpro@gmail.com>
2025-03-02 02:15:29 +0330

    fix: fix licenses

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -284,13 +284,8 @@ class UserController extends Controller
 
     public function doLadder(LadderRequest $request)
     {
-        if ($request['type'] == 'all_portfolios') {
-            return $this->userRepository->doLadder();
-        }
-        dd($request['type']);
         return match ($request['type']) {
-            'all_portfolios' => $this->doLadderPortfolios($request),
-            'some_portfolios' => $this->doLadderPortfolios($request),
+            'all_portfolios', 'some_portfolios' => $this->doLadderPortfolios($request),
             'profile' => $this->userRepository->doLadder(),
             default => $this->createError('type', Constants::LADDERING_TYPE_ERROR, 422),
         };
